bacb2157f8d0f01ec294820cbf12c598368a0042
Spitzy <spitzy.dev@gmail.com>
2020-11-09 23:07:25 -0500

    created html layout, created nav bar, and create question/answer array

---
+++ script.js
@@ -0,0 +1,54 @@
+var quizQuestions = [
+    {
+        question: "What year was Javascript created in?",
+        answer: {
+            a: "1995",  
+            b: "1996", 
+            c: "2020", 
+            d: "2015", 
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "How do you compare 2 varibales to see if they are the same type and value",
+        answer: {
+            a: "===",  
+            b: "=", 
+            c: "==", 
+            d: "!==", 
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "What do JS stand for?",
+        answer: {
+            a: "Java Soup",  
+            b: "Jail Sentence", 
+            c: "JavaScript", 
+            d: "TypeScript", 
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "Who invented JavaScript?",
+        answer: {
+            a: "Patrick Dempsey",  
+            b: "Brendan Eich", 
+            c: "Tupac", 
+            d: "Donal Trump", 
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "When would you use a for loop in JavaScrupt?",
+        answer: {
+            a: "After compling",  
+            b: "When driving your Tesla", 
+            c: "When it is time to dance", 
+            d: "Do a specfic block of code multiple times", 
+        },
+        correctAnswer:"d"
+    },
+
+]
+
